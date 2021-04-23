cfe440857820045bd28489b4ad6bd9a0b6d4a04a
孟宁 <mengning997@163.com>
2021-04-23 09:22:05 +0800

    add src/lab7.3/linktable.h.

---
+++ src/lab7.3/linktable.h
@@ -0,0 +1,63 @@
+
+/********************************************************************/
+/* Copyright (C) SSE-USTC, 2012-2021                                */
+/*                                                                  */
+/*  FILE NAME             :  linktabe.h                             */
+/*  PRINCIPAL AUTHOR      :  Mengning                               */
+/*  SUBSYSTEM NAME        :  LinkTable                              */
+/*  MODULE NAME           :  LinkTable                              */
+/*  LANGUAGE              :  C                                      */
+/*  TARGET ENVIRONMENT    :  ANY                                    */
+/*  DATE OF FIRST RELEASE :  2012/12/30                             */
+/*  DESCRIPTION           :  interface of Link Table                */
+/********************************************************************/
+
+/*
+ * Revision log:
+ *
+ * Created by Mengning,2012/12/30
+ * modified by Mengning, 2021/04/23
+ *
+ */
+
+#ifndef _LINK_TABLE_H_
+#define _LINK_TABLE_H_
+
+#define SUCCESS 0
+#define FAILURE (-1)
+
+/*
+ * LinkTable Node Head Type
+ */
+typedef struct LinkTableNode tLinkTableNodeHead;
+
+/*
+ * LinkTable Type
+ */
+typedef struct LinkTable tLinkTable;
+
+/*
+ * Create a LinkTable
+ */
+tLinkTable * CreateLinkTable();
+/*
+ * Delete a LinkTable
+ */
+int DeleteLinkTable(tLinkTable *pLinkTable);
+/*
+ * Add a LinkTableNode to LinkTable
+ */
+int AddLinkTableNode(tLinkTable *pLinkTable,tLinkTableNode * pNode);
+/*
+ * Delete a LinkTableNode from LinkTable
+ */
+int DelLinkTableNode(tLinkTable *pLinkTable,tLinkTableNode * pNode);
+/*
+ * Search a LinkTableNode from LinkTable
+ * int Conditon(tLinkTableNode * pNode,void * args);
+ */
+tLinkTableNode * SearchLinkTableNode(tLinkTable *pLinkTable, int Conditon(tLinkTableNode * pNode, void * args), void * args);
+
+#endif /* _LINK_TABLE_H_ */
+
+
